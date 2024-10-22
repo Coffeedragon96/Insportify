c3950354cf4366662d1a4cb391b36039a17c746b
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-10-22 23:46:23 +0530

    email changes

--- Insportify/settings.py
+++ Insportify/settings.py
@@ -88,11 +88,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'insportifyinfo@gmail.com'
-EMAIL_HOST_PASSWORD = 'rqhjanxgepfilmrb'
-
-# EMAIL_HOST_USER = 'noreply@insportify.com'
-# EMAIL_HOST_PASSWORD = 'raval.shrey1'
+EMAIL_HOST_USER = 'noreply@insportify.com'
+EMAIL_HOST_PASSWORD = 'lyqsnvulnotrtsxz'
 
 # Added to confirm the csrf token does not fail for Safari on iPhone
 SESSION_COOKIE_SECURE = False
