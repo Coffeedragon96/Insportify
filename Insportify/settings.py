4738cf367e0682323e2e7e6347edbcbea518a812
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-10-22 21:27:20 +0530

    minor email domain change

--- Insportify/settings.py
+++ Insportify/settings.py
@@ -85,7 +85,7 @@ AUTH_USER_MODEL = 'EventsApp.User'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.insportify.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'insportifyinfo@gmail.com'
