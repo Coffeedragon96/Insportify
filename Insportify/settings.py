6a4aef4346c4355e67c8a89c14116ddf4b3b0781
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-10-22 21:33:21 +0530

    minor change

--- Insportify/settings.py
+++ Insportify/settings.py
@@ -87,7 +87,7 @@ AUTH_USER_MODEL = 'EventsApp.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.insportify.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'insportifyinfo@gmail.com'
 EMAIL_HOST_PASSWORD = 'rqhjanxgepfilmrb'
 
